f4a9f8b37344f5240a78f80ec325a8285d1d0250
paul <paul@patchdance.com>
2017-11-11 08:50:55 -0800

    RF - run code, basic check for Usage message

--- Roman2DecTest/ProgramTest.cs
+++ Roman2DecTest/ProgramTest.cs
@@ -12,11 +12,17 @@ namespace Roman2DecTest
         public void TestNoInput()
         {
             string[] args = new string[0];
+            string result = RunWithArgs(args);
+            Assert.StartsWith("Convert", result);
+        }
+
+        public string RunWithArgs(string[] args)
+        {
             using (StringWriter sw = new StringWriter())
             {
                 Console.SetOut(sw);
                 Program.Main(args);
-                Assert.NotEmpty(sw.ToString());
+                return sw.ToString();
             }
         }
     }
