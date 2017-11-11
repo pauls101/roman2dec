48f0074277fe4b7d0094334fa3843271a3ccf9f8
paul <paul@patchdance.com>
2017-11-11 10:18:08 -0800

    G - convert single character

--- Roman2Dec/Convert.cs
+++ Roman2Dec/Convert.cs
@@ -18,6 +18,17 @@ namespace Roman2Dec
         }
         public static int CharacterToDecimal(string roman)
         {
+            switch (roman.ToUpperInvariant()[0])
+            {
+                case 'M': return 1000;
+                case 'D': return 500;
+                case 'C': return 100;
+                case 'L': return 50;
+                case 'X': return 10;
+                case 'V': return 5;
+                case 'I': return 1;
+            }
+
             return 0;
         }
     }
